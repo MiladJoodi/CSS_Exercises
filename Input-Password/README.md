95a62437c4672cde7de223aa2f09931d336b2a80
Milad Joodi <43522323+MiladJoodi@users.noreply.github.com>
2023-07-31 20:07:19 +0330

    Add files via upload

---
+++ Input-Password/README.md
@@ -0,0 +1,10 @@
+## Password Input
+
+
+
+
+### Cover:
+![](https://s29.picofile.com/file/8466511142/upload_page.JPG)
+
+My GitHub Account: [GitHub.com/MiladJoodi](https://github.com/miladjoodi)  
+**Note**: [You can go here to download a single folder or file from GitHub.com](https://minhaskamal.github.io/DownGit/#/home)
---
+++ Input-Password/script.js
@@ -0,0 +1,23 @@
+﻿const $ = document
+const eye = $.querySelector('.fa-eye')
+const inputPassword = $.querySelector('.input_password')
+const label = $.querySelector('.label')
+const container = $.querySelector('.container')
+
+eye.addEventListener('click', function () {
+    if (inputPassword.type === "password") {
+        eye.classList.remove('fa-eye')
+        eye.classList.add('fa-eye-slash')
+        inputPassword.type = 'text'
+    } else {
+        eye.classList.remove('fa-eye-slash')
+        eye.classList.add('fa-eye')
+        inputPassword.type = 'password'
+        
+    }
+})
+
+inputPassword.addEventListener('click',function(){
+    label.style.top = '27px'
+    label.style.left = '27px'
+})
